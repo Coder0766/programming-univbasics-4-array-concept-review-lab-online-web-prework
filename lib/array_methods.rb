5ad731be20b86b2fa869124a662eca2a7bbd3da5
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-30 20:27:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,7 @@ def find_element_index(array, value_to_find)
   nil
 end
 
-def find_max_value(array,interger)
+def find_max_value(array)
     x = array[0]
     array.length.times {|index|
     if array[index] > x
